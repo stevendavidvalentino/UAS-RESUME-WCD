cab0d4f5fc3a2373e39ed3fdab07f10484d4f285
unknown <stevendavid.prof@gmail.com>
2025-07-05 20:25:31 +0700

    first commit

--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -24,7 +24,7 @@ export const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
+            Resume Portofolio
           </h1>
         </motion.div>
 
@@ -33,11 +33,11 @@ export const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Providing{" "}
+            Personal{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              the best
+              Website
             </span>{" "}
-            project experience.
+            Steven
           </span>
         </motion.div>
 
@@ -45,8 +45,7 @@ export const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
+          I&apos;m a Student at Cakrawala University Majoring in Data Science.
         </motion.p>
 
         <motion.a
